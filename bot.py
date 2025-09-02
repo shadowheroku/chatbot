6138820523706347f50e42574a795873415a814f
shadowheroku <shadowheroku@gmail.com>
2025-09-02 16:57:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 API_ID = 23212132      # from https://my.telegram.org
 API_HASH = "1c17efa86bdef8f806ed70e81b473c20"
 BOT_TOKEN = "8125914573:AAG3zY96757uoIGVKSDC0y_KbifIX_uL4So"
-OPENAI_KEY = "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"
+OPENAI_KEY = "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"
 
 # Configure OpenAI
 openai.api_key = OPENAI_KEY
